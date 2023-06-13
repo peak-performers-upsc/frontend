c793fa67ed5645042658bb94e55ae10b55d91f69
Anjali19991 <anjalimago999@gmail.com>
2023-06-13 22:11:20 +0530

    hmm

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/header/header"
 import Hero from "./components/hro_section/hero"
+import Cards from "./components/courses/course"
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
     <>
     <Header></Header>
     <Hero></Hero>
-    
+    <Cards></Cards>
       
     </>
   )
